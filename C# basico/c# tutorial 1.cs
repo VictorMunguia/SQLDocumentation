74e1d19dbcd1d7423cd36b06fc4ee5ab24e32012
Victor Munguia <victor00171@hotmail.com>
2018-02-09 12:12:17 -0600

    Commit message

---
+++ C# basico/c# tutorial 1.cs
@@ -0,0 +1,7 @@
+
+//Para hacer una escritura en la consola utilizamos el metodo writeLine y ReadLine para poder escribir y leer lo que escribimos.
+Console.WriteLine("Hello World");
+Console.ReadLine();
+
+
+https://mva.microsoft.com/en-US/training-courses/c-fundamentals-for-absolute-beginners-16169?l=Y6D7PQQIC_5406218949
