b31e5b30e6fa199565f938a0a69b23629180c52c
VictorMunguia <victor00171@hotmail.com>
2018-03-13 13:25:57 -0600

    Inicio curso youtube javascript

---
+++ Curso Youtube javascript/IntroduccionHolaMundoYVariablesJavascript.js
@@ -0,0 +1,17 @@
+var Nombre;
+
+Nombre = "Uriel";
+
+var edad = 22;
+
+var precio = 12.5;
+
+console.log(precio);
+
+//Podemos delcarar variables con var sin importar el tipo de dato que sea e imprimir lo con console.log
+
+//console.log("Hola Mundo");//De esta forma podemos imprimir en la consola del navegador
+
+//alert("HolaMundo");//Mandar a llamar una alerta.
+
+document.querySelector("body").innerHTML = "Hola mundo " + Nombre; // Nos permite escribir texto en el body. utilizando el mas podemos concatenar texto y variables
