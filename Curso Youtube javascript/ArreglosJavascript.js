9d879b72e222f777f758a5be6efd6b5672fe9843
VictorMunguia <victor00171@hotmail.com>
2018-04-06 11:53:53 -0500

    Arreglos

---
+++ Curso Youtube javascript/ArreglosJavascript.js
@@ -0,0 +1,14 @@
+var arregloEjemplo = [20,15,7];//De esta forma es como se tienen que declarar arreglos, puedes almacenar cualquier cosa en el arrelgo, este guarda numeros.
+
+console.log("Elemento 0 del arreglo");
+console.log(arregloEjemplo[0]);//De esta forma accedemos a los elementos del atributo uno por uno
+
+console.log("Tamaño del arreglo");
+console.log(arregloEjemplo.length);//Este metodo nos permite sacar el tamaño del arreglo iniciando con el numeor 1
+
+
+arregloEjemplo.push(69);//Este metodo nos permite agregar un elemento al final del arreglo
+console.log("Agregando elementos al final con el metodo push");
+console.log(arregloEjemplo);//De esta forma imprimimos todo el arreglo
+
+arregloEjemplo.unshift(77);//Este metodo nos permite agregar un elemento al inicio del arreglo
